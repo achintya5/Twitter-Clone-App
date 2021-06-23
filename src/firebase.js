ee0b9ec217e01b5bda173d40243e2ca9916cd9f0
achintya5 <78315614+achintya5@users.noreply.github.com>
2021-06-23 23:15:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBY5oHKQOZJconD8X46VGrPRQFur9vwVDE",
-  authDomain: "twitter-clone-e2ecf.firebaseapp.com",
-  projectId: "twitter-clone-e2ecf",
-  storageBucket: "twitter-clone-e2ecf.appspot.com",
-  messagingSenderId: "118973184710",
-  appId: "1:118973184710:web:7ad1448e3fe323ec3435c7",
-  measurementId: "G-F0Z5GNFDMW"
+  // Enter your firebase config here.
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
